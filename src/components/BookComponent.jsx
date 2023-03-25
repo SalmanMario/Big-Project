d329e0247c4a248e0d733af4819f4c98ce572870
Mario <xmariosalman@gmail.com>
2023-03-25 12:26:46 +0200

    remove button logic

--- src/components/BookComponent.jsx
+++ src/components/BookComponent.jsx
@@ -1,7 +1,6 @@
 import {
   Button,
   Card,
-  CardActions,
   CardContent,
   CardMedia,
   createTheme,
@@ -13,7 +12,6 @@ import {
 import { green } from "@mui/material/colors";
 import { Box } from "@mui/system";
 import { useState } from "react";
-import { BookItem } from "./BookItem";
 
 export function BookComponent({ book }) {
   const [showFullText, setShowFullText] = useState(false);
@@ -42,12 +40,8 @@ export function BookComponent({ book }) {
     <ThemeProvider theme={theme}>
       <Grid sx={{ my: 2 }} item lg={3} md={4} sm={6} xs={12}>
         <Box display="flex" justifyContent="center">
-          <Card sx={{ maxWidth: 260 }}>
-            <CardMedia
-              sx={{ height: 320, objectFit: "cover", imageRendering: "high-quality" }}
-              image={book.coverImageURL}
-              title="book"
-            />
+          <Card sx={{ maxWidth: 240 }}>
+            <CardMedia sx={{ height: 320 }} image={book.coverImageURL} title="book" />
             <CardContent>
               <Typography gutterBottom variant="h4" component="div">
                 {book.title}
@@ -55,16 +49,15 @@ export function BookComponent({ book }) {
               <Typography gutterBottom variant="h6" component="div">
                 {book.author}
               </Typography>
-              <div style={{ overflow: "hidden", textOverflow: "ellipsis" }}>
-                <Typography variant="body1">
-                  {showFullText ? book.description : `${book.description.slice(0, 150)}...`}
-                </Typography>
-              </div>
-              <Grid>
+              <Typography variant="body1">
+                {showFullText ? book.description : `${book.description.slice(0, 150)}...`}
+              </Typography>
+              <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column">
                 <Button variant="contained" onClick={handleShowFullText}>
                   {showFullText ? "Hide Description" : "Show Description"}
                 </Button>
-              </Grid>
+                <Button variant="contained">View Book</Button>
+              </Box>
             </CardContent>
           </Card>
         </Box>
--- src/components/BookPost.jsx
+++ src/components/BookPost.jsx
@@ -1,9 +1,6 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Grid, Stack, Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
-import { useState } from "react";
 import { BookComponent } from "./BookComponent";
-import { BookItem } from "./BookItem";
-import { ShowButton } from "./ShowButton";
 
 export function BookPost({ bookItem }) {
   return (
